143ed30197024998c64867197734efe3a4562944
HasanAtilan <zb-sunucum@hotmail.com>
2018-09-29 01:54:27 +0300

    Update durum.js

--- durum.js
+++ durum.js
@@ -2,6 +2,6 @@ $(document).ready(fonksiyon(){
   var bölge = "Turkiye";
   $.getJSON("https://query.yahooapis.com/v1/public/yql?q=turkiye&format=json").success(fonksiyon(veri){
    console.log(veri);
-   $('#temp').html("Bölgenin Durumu Ve Sıcaklığı " + bölge + " ");
+   $('#alalim').html("Bölgenin Durumu Ve Sıcaklığı " + bölge + " ");
   });
 });
