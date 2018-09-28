39dcc4aa637a58f42ce1ce1d2be5942ac21cf5da
HasanAtilan <zb-sunucum@hotmail.com>
2018-09-29 01:44:08 +0300

    Update durum.js

--- durum.js
+++ durum.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
   var bölge = "Turkiye";
-  var bolgeara = "select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + bölge + "') and u='c'"
-  $.getJSON("https://query.yahooapis.com/v1/public/yql?q=" + bolgeara + "&format=json").success(function(veri){
+  $.getJSON("https://query.yahooapis.com/v1/public/yql?q=turkiye&format=json").success(function(veri){
    console.log(veri);
-   $('#temp').html("Bölgenin Durumu Ve Sıcaklığı " + bölge + " is " + veri.query.results.channel.item.condition.temp + "°C");
+   $('#temp').html("Bölgenin Durumu Ve Sıcaklığı " + bölge + " "°C");
   });
 });
